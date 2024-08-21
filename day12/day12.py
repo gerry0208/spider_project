93a4c1f7b3c2c79c8c2febe078c349f90674692c
gerry0208 <131978751+gerry0208@users.noreply.github.com>
2024-08-21 19:04:21 +0800

    Add files via upload

---
+++ day12/day12.py
@@ -0,0 +1,28 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+from chaojiying import Chaojiying_Client
+
+driver = webdriver.Chrome()
+driver.maximize_window()
+driver.get('https://www.chaojiying.com/user/login/')
+time.sleep(1)
+# 输入账户名和密码
+driver.find_element(By.XPATH, '//input[@name="user"]').send_keys('gerrychan')
+time.sleep(1)
+driver.find_element(By.XPATH, '//input[@type="password"]').send_keys('gerrychan*')
+# 获取验证码图片
+pic = driver.find_element(By.XPATH, '/html/body/div[3]/div/div[3]/div[1]/form/div/img')
+pic.screenshot('验证码.png')
+# 发送图片给超级鹰，获取json数据
+chaojiying = Chaojiying_Client('gerrychan', 'gerrychan*', '962323')
+img = open('验证码.png', 'rb').read()
+data = chaojiying.PostPic(img, 1902)
+# 填入验证码并登录
+driver.find_element(By.XPATH, '/html/body/div[3]/div/div[3]/div[1]/form/p[3]/input').send_keys(data['pic_str'])
+time.sleep(1)
+driver.find_element(By.XPATH, '//input[@type="submit"]').click()
+
+time.sleep(3)

16060e36c3b47133984ef67303a77586b4bc71cd
gerry0208 <2528548314@qq.com>
2024-08-25 18:34:07 +0800

    更新了

---
+++ day29/douban_movie/douban_movie/pipelines.py
@@ -0,0 +1,26 @@
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+
+
+# useful for handling different item types with a single interface
+from itemadapter import ItemAdapter
+from scrapy import Request
+from scrapy.pipelines.images import ImagesPipeline
+
+
+# class DoubanMoviePipeline:
+#     def process_item(self, item, spider):
+#         return item
+
+
+class DoubanMovieImagesPipeline(ImagesPipeline):
+    def get_media_requests(self, item, info):
+        urls = ItemAdapter(item).get(self.images_urls_field, [])
+        return [Request(u, meta={"image": item}) for u in urls]
+
+    def file_path(self, request, response=None, info=None, *, item=None):
+        item = request.meta.get("image")
+        image_name = item["image_names"]
+        return f"./douban_movie_covers/{image_name}.jpg"
---
+++ day29/douban_movie/douban_movie/spiders/movie.py
@@ -0,0 +1,38 @@
+import scrapy
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
+from ..items import DoubanMovieItem
+
+
+class MovieSpider(CrawlSpider):
+    name = "movie"
+    allowed_domains = ["movie.douban.com"]
+    start_urls = ["https://movie.douban.com/top250"]
+
+    rules = (
+        Rule(
+            LinkExtractor(restrict_xpaths='//div[@class="hd"]/a'),
+            callback="parse_item",
+            follow=False,
+        ),
+        Rule(
+            LinkExtractor(
+                restrict_xpaths='//*[@id="content"]/div/div[1]/div[2]/span[3]/a'
+            ),
+            callback="parse_item",
+            follow=True,
+        ),
+    )
+
+    def parse_item(self, response):
+        item = DoubanMovieItem()
+        img_urls = response.xpath('//a[@class="nbgnbg"]/img/@src').getall()
+        item["image_urls"] = img_urls
+        img_names = response.xpath('//span[@property="v:itemreviewed"]/text()').get()
+        try:
+            img_names = img_names.replace(":", "：")
+        except:
+            pass
+        else:
+            item["image_names"] = img_names
+            return item
